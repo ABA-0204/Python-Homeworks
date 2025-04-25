e7deb4a30bc640e8e18e591819bb31ba9e5f487e
ABA-0204 <aba8504@gmail.com>
2025-04-25 18:38:03 +0500

    Update and rename homework.ipynb to homework.py

---
+++ lesson-1/homework/homework.py
@@ -0,0 +1,23 @@
+#Given a side of square. Find its perimeter and area.
+#Given diameter of circle. Find its length.
+#Given two numbers a and b. Find their mean.
+#Given two numbers a and b. Find their sum, product and square of each number.
+
+
+    P = 4 * side
+    S = side ** 2
+   print(P) 
+#Given diameter of circle. Find its length.
+
+    C = math.pi * diameter
+    print(C)
+#Given two numbers a and b. Find their mean.
+
+    mean = (a + b) / 2
+    print(mean)
+#Given two numbers a and b. Find their sum, product and square of each number.
+    sumtotal = a + b
+    product = a * b
+    square_a = a ** 2
+    square_b = b ** 2
+    print(sum, product, square_a, square_b)
