b8e68e877c6d4e1fe638b33a81701c49bf5664db
ABA-0204 <aba8504@gmail.com>
2025-04-25 18:45:21 +0500

    Update homework.py

--- lesson-1/homework/homework.py
+++ lesson-1/homework/homework.py
@@ -3,21 +3,21 @@
 #Given two numbers a and b. Find their mean.
 #Given two numbers a and b. Find their sum, product and square of each number.
 
-
-    P = 4 * side
-    S = side ** 2
-   print(P) 
+side=3
+P = 4 * side
+S = side ** 2
+print(P) 
 #Given diameter of circle. Find its length.
-
-    C = math.pi * diameter
-    print(C)
+diameter=15
+C = math.pi * diameter
+print(C)
 #Given two numbers a and b. Find their mean.
 
-    mean = (a + b) / 2
-    print(mean)
+mean = (a + b) / 2
+print(mean)
 #Given two numbers a and b. Find their sum, product and square of each number.
-    sumtotal = a + b
-    product = a * b
-    square_a = a ** 2
-    square_b = b ** 2
-    print(sum, product, square_a, square_b)
+sumtotal = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(sum, product, square_a, square_b)
